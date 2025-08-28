58ed697c19a6270c500767603af4e9c77f34a83a
shimoligupta <sgmg197776@gmail.com>
2025-08-28 10:06:07 +0530

    DAY 4 TASK

---
+++ node/index.js
@@ -0,0 +1,13 @@
+// Import http module
+const http = require('http');
+
+// Create server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+});
+
+// Start server
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
